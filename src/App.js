13524e6c1f14f10d7a0aac0af7c297fbc75e2988
Nikita Khristinin <nkhristinin@gmail.com>
2021-06-01 12:58:42 +0200

    Some eslint fixes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
-import { useEffect, useState, useCallback } from "react";
-import debounce from "lodash.debounce";
 import SearchForm from "modules/Search/components/SearchForm";
 import "./App.css";
-
-const PLACE_TYPE = {
-  CITY: "C",
-  AIRPORT: "A",
-  DISTRICT: "D",
-};
-const getAddress = (items) => items.filter((x) => x).join(", ");
-
-const transformSearch = (search) => {
-  const { name, region, city, country, placeType } = search;
-  const newResult = {
-    name,
-    placeType,
-  };
-
-  let address = getAddress([region, country]);
-
-  if (placeType !== PLACE_TYPE.CITY && city) {
-    address = getAddress([city, address]);
-  }
-
-  newResult.address = address;
-
-  return newResult;
-};
 
 function App() {
   return (
